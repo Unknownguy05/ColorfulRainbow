2045a1584b540ce1305e395bf028c567331d5435
codewithSUKU <72142656+codewithSUKU@users.noreply.github.com>
2020-10-03 00:13:26 +0530

    Change Coding and Creat Flower

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from turtle import *
-colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
-for x in range(360):
-    pencolor(colors[x % 20])
-    width(x / 100 + 1)
-    forward(x)
-    left(5)
+import turtle
+def draw_square(angie):
+    for i in range(0,4):
+        angie.forward(100)
+        angie.right(90)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("blue")
+
+    brad = turtle.Turtle()
+    brad.shape("arrow")
+    brad.color("orange")
+    brad.speed(10)
+    
+    for i in range(0,36):
+        draw_square(brad)
+        brad.right(10)
+
+    window.exitonclick()
+
+         
+draw_art()
